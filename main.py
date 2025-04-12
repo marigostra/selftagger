3d1e7b30de6ef8cd4d2bbc585976da548e039aee
Michael Pozhidaev <msp@luwrain.org>
2025-04-12 11:37:17 +0700

    Adding MultiheadAttention

--- main.py
+++ main.py
@@ -12,40 +12,55 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 
-
+import torch
+from torch import tensor
+import torch.nn as nn
+from torch.nn import MultiheadAttention as MHAtten
 import re
+
 
 word_re = re.compile("^(\\d+)\t([^\\t]+)\\t([^\\t]+)\\t([A-Z]+)\\t(.*)$")
 
-line = "7	Якобсона	Якобсон	PROPN	_	Animacy=Anim|Case=Gen|Gender=Masc|Number=Sing	6	flat:name	6:flat:name	SpaceAfter=No"
-m = word_re.match(line)
-
-
-
-with open("/x/cent") as f:
-    lines = f.readlines()
-sents = []
-sent_words = []
-sent = None
-words = []
-for l in lines:
-    if l.startswith("# text ="):
-        if sent is not None:
-            sents.append(sent)
-            sent_words.append(words)
-        sent = l[len("# text = "):]
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, file_name):
+        self.sents = []
+        self.words = []
+        with open(file_name) as f:
+            lines = f.readlines()
+        sent = None
         words = []
-        continue
-    if l.startswith("#"):
-        continue
+        for l in lines:
+            if l.startswith("# text ="):
+                if sent is not None:
+                    self.sents.append(sent)
+                    self.words.append(words)
+                sent = l[len("# text = "):]
+                words = []
+                continue
+            if l.startswith("#"):
+                continue
+            r = word_re.match(l)
+            if not r:
+                continue
+            words.append(r.group(2))
+            assert len(words) == int(r.group(1)), str(len(words)) + " not equals " + r.group(1)
+        assert len(self.sents) == len(self.words)
 
-    r = word_re.match(l)
-    if not r:
-        continue
-    words.append(r.group(2))
-    assert len(words) == int(r.group(1)), str(len(words)) + " not equals " + r.group(1)
-cent = None
-words = None
-assert len(sents) == len(sent_words)
+class Model(nn.Module):
+    def __init__(self, input_dim, output_dim, num_heads):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.num_heads = num_heads
+        self.ln_query = nn.Linear(self.input_dim, self.output_dim)
+        self.ln_key = nn.Linear(self.input_dim, self.output_dim)
+        self.ln_value = nn.Linear(self.input_dim, self.output_dim)
+        self.atten = MHAtten(self.output_dim, self.num_heads)
 
-print(len(sents))
+    def forward(self, x):
+        q = self.ln_query(x)
+        k = self.ln_key(x)
+        v = self.ln_value(x)
+        return self.atten(q, k, v)        
+
+m = Model(128, 64, 8)
